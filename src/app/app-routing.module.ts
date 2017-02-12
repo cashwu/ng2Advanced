34a99f222dce6ba080b106e74b42e48f470fdd7f
Cash Wu <cash@cashwu.com>
2017-02-12 13:53:08 +0800

    08 加入 Charts 子路由與左側選單套版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { RickshawComponent } from './rickshaw/rickshaw.component';
+import { RichshawComponent } from './richshaw/richshaw.component';
+import { RadialComponent } from './radial/radial.component';
+import { FlotComponent } from './flot/flot.component';
 import { CardsComponent } from './cards/cards.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule, Component } from '@angular/core';
@@ -5,17 +9,15 @@ import {Route, Routes,  RouterModule} from '@angular/router';
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
-  {
-    path: '',
-    component : DashboardComponent
-  },
-  {
-    path: 'dashboard',
-    component : DashboardComponent
-  },
-  {
-    path: 'cards',
-    component : CardsComponent
+  { path: '', component : DashboardComponent  },
+  { path: 'dashboard', component : DashboardComponent },
+  { path: 'cards', component : CardsComponent },
+  { path : "charts",
+    children : [
+      { path : "flot", component : FlotComponent },
+      { path : "radial", component : RadialComponent },
+      { path : "rickshaw", component : RickshawComponent }
+    ]
   },
   fallbackRoute
 ];
---
+++ src/app/richshaw/richshaw.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-richshaw',
+  templateUrl: './richshaw.component.html',
+  styleUrls: ['./richshaw.component.css']
+})
+export class RichshawComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
