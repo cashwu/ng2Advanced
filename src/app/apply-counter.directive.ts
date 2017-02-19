3cf664a1e4a6c9f1e930ad6c04e8fda98e466677
Cash Wu <cash@cashwu.com>
2017-02-19 16:02:30 +0800

    directive

--- src/app/apply-counter.directive.ts
+++ src/app/apply-counter.directive.ts
@@ -1,10 +1,11 @@
-import {HostListener, HostBinding,  Directive} from '@angular/core';
+import {ElementRef, Renderer,  Input,   HostListener,    HostBinding,     Directive} from '@angular/core';
 
 @Directive({
   selector: '.pt0 [apply-counter]'
 })
 export class ApplyCounterDirective {
 
+  @Input()
   @HostBinding("class.bg-success")
   bgClass = false;
 
@@ -13,10 +14,16 @@ export class ApplyCounterDirective {
     this.bgClass = !this.bgClass;
   }
 
-  constructor() { 
-    setTimeout(() => {
-      this.bgClass = true;
-    }, 5000);
+  constructor(private el : ElementRef, private renderer : Renderer) { 
+    // setTimeout(() => {
+    //   this.bgClass = true;
+    // }, 5000);
+  }
+
+  ngOnInit(){
+    (this.el.nativeElement as HTMLDivElement).innerHTML = "123";
+
+    this.renderer.setElementClass(this.el.nativeElement, "bg-danger", true);
   }
 
 }
